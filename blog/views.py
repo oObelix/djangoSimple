cb8a705ce33b979b2bdc5a76d7a65fd4ab4fc3b9
oObelix <viacheslav.sabadash@wecandevelopit.com>
2021-08-13 23:58:59 +0300

    views modification with Post QuerySet

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,20 @@
 import os
 from django.conf import settings
+from django.db.models import QuerySet
 from django.shortcuts import render
+from .models import Post
+from django.utils import timezone
 
 
 def post_list(request):
+    posts: QuerySet = Post.objects.filter(
+        published_date__lte=timezone.now()
+    ).order_by("published_date")
     return render(request=request,
                   template_name=os.path.join(
                       settings.BASE_DIR,
                       "blog", "templates", "blog", "post_list.html"
                   ),
-                  context={})
+                  context={
+                      'posts': posts
+                  })

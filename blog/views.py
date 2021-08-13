e7df691b6c98683558c34788464f7a9cb2d8f4aa
oObelix <viacheslav.sabadash@wecandevelopit.com>
2021-08-13 23:23:40 +0300

    added view and empty template post_list.html

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,12 @@
+import os
+
+from django.conf import settings
 from django.shortcuts import render
 
-# Create your views here.
+
+def post_list(request):
+    return render(request=request,
+                  template_name=
+                  os.path.join(settings.BASE_DIR,
+                               "blog/templates/blog/post_list.html"),
+                  context={})

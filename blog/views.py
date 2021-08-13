56c08ce28383709381239d094c1f277b96a2b4d6
oObelix <viacheslav.sabadash@wecandevelopit.com>
2021-08-13 23:51:26 +0300

    fix views.py with os.path.join()

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,12 @@
 import os
-
 from django.conf import settings
 from django.shortcuts import render
 
 
 def post_list(request):
     return render(request=request,
-                  template_name=
-                  os.path.join(settings.BASE_DIR,
-                               "blog/templates/blog/post_list.html"),
+                  template_name=os.path.join(
+                      settings.BASE_DIR,
+                      "blog", "templates", "blog", "post_list.html"
+                  ),
                   context={})

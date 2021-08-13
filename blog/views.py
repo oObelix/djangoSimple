11141d562d8068f3febae5b5aec9a729ecf96d80
oObelix <viacheslav.sabadash@wecandevelopit.com>
2021-08-14 01:06:36 +0300

    added detailed post read

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 import os
 from django.conf import settings
 from django.db.models import QuerySet
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.utils import timezone
 
@@ -18,3 +18,13 @@ def post_list(request):
                   context={
                       'posts': posts
                   })
+
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request=request,
+                  template_name=os.path.join(
+                      settings.BASE_DIR,
+                      "blog", "templates", "blog", "post_detail.html"
+                  ),
+                  context={'post': post})
